efbf5bf669056422ee466bfa712997d45bc0f2af
acharya.utsav.15@gmail.com <uutsavacharya15@Utsavs-Air.home>
2021-12-31 16:53:46 +0000

    Complete

---
+++ index.js
@@ -0,0 +1,69 @@
+
+// Detecting Button Press
+var drums = document.querySelectorAll(".drum");
+
+var n = 0;
+while (n <= 6) {
+  drums[n].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+  });
+  n++;
+}
+
+
+// Detecting Keyboard Press
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+});
+
+
+
+function makeSound(key){
+
+  switch (key) {
+
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(key);
+
+  }
+
+}
